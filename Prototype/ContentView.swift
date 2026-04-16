6ad8de6c8ac6b5d688c8b784c0a8888ce08b0beb
Leslie Segovia <segovialeslie1030@gmail.com>
2026-04-16 12:39:50 -0700

    Initial Commit

---
+++ Prototype/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Prototype
+//
+//  Created by Marisol Bueno on 4/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Prototype/PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeApp.swift
+//  Prototype
+//
+//  Created by Marisol Bueno on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
